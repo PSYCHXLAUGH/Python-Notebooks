23fc44d248f27c72658dbab8f8a543671db259a1
psychx <xxx@psychx.x>
2023-07-04 07:11:56 +0300

    iterator

---
+++ something/iterators.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from typing import Union
+
+class SimpleIterator:
+    def __init__(self, counter):
+        self.counter = counter
+
+
+    def __iter__(self):
+        self.init_value = 0
+        return self
+
+
+    def __next__(self) -> int:
+        if self.init_value < self.counter:
+            self.init_value += 1
+            return self.init_value
+        raise StopIteration
+
+
+if __name__ == "__main__":
+    test_iter = SimpleIterator(10)
+    print(list(test_iter))
+    print(list(test_iter))
+    for x in test_iter:
+        print(x)
