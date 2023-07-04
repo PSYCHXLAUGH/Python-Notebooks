eafa2021c8d6fea072ca140d8587cea1912c3886
psychx <xxx@psychx.x>
2023-07-04 08:03:49 +0300

    upd

--- something/somepattern.py
+++ something/somepattern.py
@@ -2,10 +2,10 @@
 
 class Test:
 
-    attrs = {"someshit": 1}
+    __attrs = {"someshit": 1}
 
     def __init__(self) -> None:
-        self.__dict__ = self.attrs
+        self.__dict__ = self.__attrs
 
 
 print(Test().__dict__ is Test().__dict__)
